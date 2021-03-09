4c66d972e8a0a711a90b1a09caf35dca4ee5250b
Leonardo-wl <leonardo.alonsot@hotmail.com>
2021-03-09 14:46:43 -0300

    Projeto criado

---
+++ src/main/java/com/leonardoalonso/springbootionicbackend/SpringbootIonicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.leonardoalonso.springbootionicbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIonicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIonicBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardoalonso/springbootionicbackend/SpringbootIonicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardoalonso.springbootionicbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIonicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
