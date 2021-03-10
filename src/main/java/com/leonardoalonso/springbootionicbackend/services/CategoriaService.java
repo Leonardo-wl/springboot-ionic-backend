1d25d2475728c65bbc144e7e90e27153017f2782
Leonardo-wl <leonardo.alonsot@hotmail.com>
2021-03-10 15:34:26 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/leonardoalonso/springbootionicbackend/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.leonardoalonso.springbootionicbackend.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leonardoalonso.springbootionicbackend.domain.Categoria;
+import com.leonardoalonso.springbootionicbackend.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
