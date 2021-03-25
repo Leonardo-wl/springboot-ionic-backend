c687622c8640994a05883cbbecd673e09cdcb39e
Leonardo-wl <leonardo.alonsot@hotmail.com>
2021-03-25 16:45:32 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/leonardoalonso/springbootionicbackend/config/DevConfig.java
+++ src/main/java/com/leonardoalonso/springbootionicbackend/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.leonardoalonso.springbootionicbackend.services.DBService;
+import com.leonardoalonso.springbootionicbackend.services.EmailService;
+import com.leonardoalonso.springbootionicbackend.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
