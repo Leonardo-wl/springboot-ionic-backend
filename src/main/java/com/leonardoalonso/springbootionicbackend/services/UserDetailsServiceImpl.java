1891def7a752325b12c942c2e67647ff4587f484
Leonardo-wl <leonardo.alonsot@hotmail.com>
2021-04-06 03:31:24 -0300

    Implementando autenticacao e geracao de token JWT

---
+++ src/main/java/com/leonardoalonso/springbootionicbackend/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.leonardoalonso.springbootionicbackend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.leonardoalonso.springbootionicbackend.domain.Cliente;
+import com.leonardoalonso.springbootionicbackend.repositories.ClienteRepository;
+import com.leonardoalonso.springbootionicbackend.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+}
