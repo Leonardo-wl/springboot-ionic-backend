c6fd1c54e72d13e1ffe12034039162583e356a32
Leonardo-wl <leonardo.alonsot@hotmail.com>
2021-04-06 05:48:49 -0300

    Esqueci a senha

--- src/main/java/com/leonardoalonso/springbootionicbackend/services/EmailService.java
+++ src/main/java/com/leonardoalonso/springbootionicbackend/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.leonardoalonso.springbootionicbackend.domain.Cliente;
 import com.leonardoalonso.springbootionicbackend.domain.Pedido;
 
 public interface EmailService {
@@ -15,4 +16,6 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 
 	void sendHtmlEmail(MimeMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
