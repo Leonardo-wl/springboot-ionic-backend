3062c1c79298e0eed7dc59c0ab96e73fd7710716
Leonardo-wl <leonardo.alonsot@hotmail.com>
2021-04-07 04:25:36 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/leonardoalonso/springbootionicbackend/SpringbootIonicBackendApplication.java
+++ src/main/java/com/leonardoalonso/springbootionicbackend/SpringbootIonicBackendApplication.java
@@ -1,11 +1,17 @@
 package com.leonardoalonso.springbootionicbackend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.leonardoalonso.springbootionicbackend.services.S3Service;
+
 @SpringBootApplication
 public class SpringbootIonicBackendApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 
 	public static void main(String[] args) {
@@ -13,6 +19,7 @@ public class SpringbootIonicBackendApplication implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String... args) throws Exception {		
+	public void run(String... args) throws Exception {	
+		s3Service.uploadFile("D:\\Léo\\teste.jpg");
 	}	
 }
