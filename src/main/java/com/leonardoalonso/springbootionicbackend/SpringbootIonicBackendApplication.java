0485fbc32816ebb43c5366d8ef3f3d91469443cd
Leonardo-wl <leonardo.alonsot@hotmail.com>
2021-04-07 04:56:17 -0300

    Enviando imagem via endpoint

--- src/main/java/com/leonardoalonso/springbootionicbackend/SpringbootIonicBackendApplication.java
+++ src/main/java/com/leonardoalonso/springbootionicbackend/SpringbootIonicBackendApplication.java
@@ -1,25 +1,17 @@
 package com.leonardoalonso.springbootionicbackend;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.leonardoalonso.springbootionicbackend.services.S3Service;
-
 @SpringBootApplication
 public class SpringbootIonicBackendApplication implements CommandLineRunner {
 	
-	@Autowired
-	private S3Service s3Service;
-	
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootIonicBackendApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {	
-		s3Service.uploadFile("D:\\Léo\\teste.jpg");
 	}	
 }
